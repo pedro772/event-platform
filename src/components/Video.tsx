40d6523ec4e237b229281cf5b8a46fba452403e1
pedro772 <plucena17230@gmail.com>
2022-06-24 21:58:47 -0300

    Aula 05 - Ignite Lab

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,49 +1,22 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 import "@vime/core/themes/default.css";
 
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug ($slug: String) {
-    lesson(where: {slug: $slug}) {
-      title
-      id
-      description
-      teacher {
-        avatarURL
-        bio
-        name
-      }
-    }
-  }
-`
 
 interface VideoProps {
   lessonSlug: string
 }
 
-interface GetLessonBySlugResponse {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    }
-  }
-}
-
 export function Video( props: VideoProps ) {
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: props.lessonSlug
     }
   });
 
-  if(!data) {
+  if(!data || !data.lesson) {
     return <p className="flex-1">Carregando...</p>
   }
 
@@ -52,7 +25,7 @@ export function Video( props: VideoProps ) {
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId={data.lesson.videoId} />
+            <Youtube videoId={data.lesson.videoId} key={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -68,18 +41,20 @@ export function Video( props: VideoProps ) {
               {data.lesson.description}
             </p>
 
-            <div className="flex items-center gap-4 mt-6">
-              <img 
-                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src={data.lesson.teacher.avatarURL}
-                alt="Foto do Professor"
-              />
+            { data.lesson.teacher &&  (
+              <div className="flex items-center gap-4 mt-6">
+                <img 
+                  className="h-16 w-16 rounded-full border-2 border-blue-500"
+                  src={data.lesson.teacher.avatarURL}
+                  alt="Foto do Professor"
+                />
 
-              <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                <div className="leading-relaxed">
+                  <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                  <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-4">
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client"
 
 export const client = new ApolloClient({
   uri: import.meta.env.VITE_API_URL,
